63fafdbbdb09fcf2d91bf105966e7fd3eeaded04
MD.ZIAUL HASHAN RIFAT <mzhripat@gmail.com>
2025-01-29 01:47:07 +0600

    first commit

---
+++ main.py
@@ -0,0 +1,29 @@
+#Mean
+"""
+import numpy
+
+speed=[99,86,87,88,111,86,103,87,94,78,77,85,86]
+
+x=numpy.mean(speed)
+print(x)
+
+#Medium
+
+import numpy
+
+speed=[99,86,87,88,111,86,103,87,94,78,77,85,86]
+
+x=numpy.median(speed)
+print(x)
+"""
+#Mode
+
+import statistics
+
+speed=[99,86,87,88,111,86,103,87,94,78,77,85,86]
+
+x=statistics.mode(speed)
+print(x)
+
+
+
